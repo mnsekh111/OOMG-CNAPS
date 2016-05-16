1ef02b9a171f631303e8210e01e8b49454726654
mnsekh111 <mnsekh111@gmail.com>
2016-05-16 01:10:01 -0400

    revert

--- WEB-INF/classes/servlet/Trajectory.java
+++ WEB-INF/classes/servlet/Trajectory.java
@@ -1,196 +1,242 @@
 package servlet;
 
-import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import javax.imageio.ImageIO;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import util.*;
 
-public class Transection extends HttpServlet {
+import util.Global;
+import util.Log;
+import util.RandomID;
 
-    /**
-     * Constructor of the object.
-     */
-    public Transection() {
-        super();
-    }
+public class Trajectory extends HttpServlet {
 
-    /**
-     * The doGet method of the servlet. <br>
-     *
-     * This method is called when a form has its tag value method equals to get.
-     *
-     * @param request the request send by the client to the server
-     * @param response the response send by the server to the client
-     * @throws ServletException if an error occurred
-     * @throws IOException if an error occurred
-     */
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	/**
+	 * Constructor of the object.
+	 */
+	public Trajectory() {
+		super();
+	}
 
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 *
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
 
-        response.setContentType("text/html");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
 
-        String dir=Global.dir+"transect/";
-        String ID=RandomID.getID();
+		List<String>lonlats =new LinkedList<String>();
+		int n=Integer.valueOf( request.getParameter("n"));
 
+		//		for(int i=0;i<Integer.valueOf(n);i++){
+		//			out.print(request.getParameter("lon"+String.valueOf(i)));
+		//			out.print(" ");
+		//			out.print(request.getParameter("lat"+String.valueOf(i)));
+		//			out.println();
+		//		}
 
-        String imagename=dir+Global.output_section+ID+Global.output_section_type;
+		if (n<1) return ;
 
-        String lon1,lon2,lat1,lat2,vname,date,time;
+		String ID=RandomID.getID();
+        String dir = Global.dir+"trajectory/";
 
-        vname=request.getParameter("vname");
-        lon1=request.getParameter("lon1");
-        lat1=request.getParameter("lat1");
-        lon2=request.getParameter("lon2");
-        lat2=request.getParameter("lat2");
-        date=request.getParameter("date");
-        time=request.getParameter("time");
+		FileWriter input=new FileWriter(dir+Global.input_drift+ID);
 
+		for (int i=0;i<n;i++){
+			input.write(request.getParameter("lon"+String.valueOf(i)));
+			input.write(" ");
+			input.write(request.getParameter("lat"+String.valueOf(i)));
+			input.write("\n");
+		}
+		input.close();
 
+		Process p;
 
-        ServletContext sc = getServletContext();
+		try{
+			String line;
+			 p=Runtime.getRuntime().exec("bash "+Global.bash_drift+" "+ID,null,new File(dir));
+		      BufferedReader reader =
+		          new BufferedReader
+		            (new InputStreamReader(p.getInputStream()));
+		        while ((line = reader.readLine()) != null ) {
+		        		//new Log().log(line);
+		        		if (line=="end\n")
+		        			break;
+		        		//Thread.sleep(1000);
+		        }
 
+/*			 BufferedReader reader_e =
+			          new BufferedReader
+			            (new InputStreamReader(p.getErrorStream()));
+			        while ((line = reader_e.readLine()) != null ) {
+			        		new Log().log("error "+line);
+			        }*/
 
-        try{
-            FileWriter f_vname=new FileWriter(dir+Global.input_section_1+ID);
-            f_vname.write(vname+"\n");
-            f_vname.close();
+		    reader.close();
+		   // reader_e.close();
+/*		    if (timeout==Global.MaxTimeout){
+		    	//p.destroy();
+			    BufferedWriter writer= new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+			    writer.write("exit\n");
+			    writer.close();
+		    	return;
+		    }*/
 
-            FileWriter log = new FileWriter("/home/smnatara/log");
-            log.write(dir+Global.input_section_1+ID+"\n");
+		}catch(Exception e){
+			new Log().log(e.getMessage());
 
-            FileWriter f_lon_lat_sectoin=new FileWriter(dir+Global.input_section_2+ID);
-            f_lon_lat_sectoin.write(lon1+" "+lat1+"\n");
-            f_lon_lat_sectoin.write(lon2+" "+lat2+"\n");
-            f_lon_lat_sectoin.close();
+			return;
+		}
 
-            FileWriter f_sectiontime=new FileWriter(dir+Global.input_section_3+ID);
-            f_sectiontime.write(date.substring(0, 4)+" "+date.substring(4, 6)+" "+date.substring(6)+" "+time+"\n");
-            f_sectiontime.close();
-
-            int timeout=0;
-            String line;
-            Process p = Runtime.getRuntime().exec("bash "+Global.bash_section+" "+ID, null, new File(dir));
-            log.write("bash "+Global.bash_section+" "+ID+"\n");
-            log.write(dir+Global.output_section+ID+Global.output_section_type);
-            log.close();
-
-
-            BufferedReader reader_e =
-                    new BufferedReader
-                            (new InputStreamReader(p.getErrorStream()));
-            while ((line = reader_e.readLine()) != null ) {
-                //Thread.sleep(1000);
-                //new Log().log(line);
-                timeout++;
-                //System.out.println(line);
-            }
-
-
-
-            BufferedReader reader =
-                    new BufferedReader
-                            (new InputStreamReader(p.getInputStream()));
-            while ((line = reader.readLine()) != null ) {
-                //Thread.sleep(1000);
-                timeout++;
-                System.out.println(line);
-            }
-
-            p.waitFor();
-
-//
-//
-//
-            reader.close();
-
-//		    if (timeout==Global.MaxTimeout){
-//		    	//p.destroy();
-//			    BufferedWriter writer= new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
-//			    writer.write("exit\n");
-//			    writer.close();
-//		    	return;
-//		    }
-        }catch(Exception e)
-        {
-            e.printStackTrace();
-        }
+		File file=new File(dir+Global.output_drift+ID+Global.output_drift_type);
 
 
-
-        // Get the MIME type of the image
-        String mimeType = sc.getMimeType(imagename);
-        if (mimeType == null) {
-            sc.log("Could not get MIME type of "+imagename);
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            return;
-        }
-
-        // Set content type
-        response.setContentType(mimeType);
-
-        // Set content size
-
-
-        File file = new File(imagename);
-
-        //busy waiting for the image
-        int timeout=0;
-        while(!file.exists() && timeout<Global.MaxTimeout )
-        {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            timeout++;
-        }
-        if (timeout>=Global.MaxTimeout)	return;
-
-        response.setContentLength((int)file.length());
-
-        // Open the file and output streams
-        FileInputStream in = new FileInputStream(file);
-
-        // Copy the contents of the file to the output stream
-        byte[] buf = new byte[2048];
-        int count = 0;
-        ServletOutputStream sout= response.getOutputStream();
-        while ((count = in.read(buf)) >= 0) {
-            sout.write(buf, 0, count);
-        }
-
-        in.close();
+/*
+ * This snippet of code is WRONG because the existence of output file doesn't mean all the result have been computed and written to the file
+ *
+ 		int timeout=0;
+		while(!file.exists() && timeout<Global.MaxTimeout )
+		{
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			timeout++;
+			new Log().log(String.valueOf(timeout));
+		}
+		if (timeout>=Global.MaxTimeout)
+		{
+		    BufferedWriter writer= new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+		    writer.write("exit\n");
+		    writer.close();
+			return;
+		}*/
 
 
-        //new File(dir+Global.input_section_1+ID).delete();
-        //new File(dir+Global.input_section_2+ID).delete();
-        //new File(dir+Global.input_section_3+ID).delete();
+		response.setContentLength((int)file.length());
+		//new Log().log("length "+String.valueOf(file.length()));
+
+		BufferedReader br=new BufferedReader(new FileReader(file));
+		String line;
+		Pattern p1=Pattern.compile("(\\S+)\\s+(\\S+)\\s+");
+
+		while((line=br.readLine())!=null){
+			//new Log().log("line "+line);
+			if (line.length()==0)
+				out.println();
+			else
+			{
+				Matcher matcher= p1.matcher(line);
+				if (matcher.find()){
+					//			    for (int i=1; i<=matcher.groupCount(); i++) {
+					//			        String groupStr = matcher.group(i);
+					//			        out.print(groupStr+" ");
+					//			    }
+					out.print(matcher.group(1)+",");
+					out.print(matcher.group(2)+";");
+				}
+			}
+
+		}
 
 
-        //file.delete();
+		br.close();
 
-        sout.close();
-
-
-
+		//new File(dir+Global.input_drift+ID).delete();
+		//file.delete();
 
 
-    }
+		out.flush();
+		out.close();
+
+		/*
+		String lon1,lat1;
+		lon1=request.getParameter("lon1");
+		lat1=request.getParameter("lat1");
+
+		FileWriter input=new FileWriter(dir+Global.input_tracking);
+		input.write(lon1+" "+lat1+"\n");
+		input.close();
+
+		try{
+		Process p = Runtime.getRuntime().exec("bash "+Global.bash_tracking, null, new File(dir));
+		}catch (Exception e){
+			out.println("Process error");
+			out.println(e);
+		}
+
+		File file=new File(dir+Global.output_tracking);
+
+	    //busy waiting for the image
+	    int timeout=0;
+	    while(!file.exists() && timeout<Global.MaxTimeout )
+	    {
+	    	try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	    	timeout++;
+	    }
+	    if (timeout>=Global.MaxTimeout)
+	    	return;
+
+	    response.setContentLength((int)file.length());
+
+	    BufferedReader br=new BufferedReader(new FileReader(file));
+	    String line;
+	    Pattern p=Pattern.compile("(\\S+)\\s+(\\S+)\\s+");
+
+	    while((line=br.readLine())!=null){
+	    		Matcher matcher= p.matcher(line);
+				if (matcher.find()){
+//				    for (int i=1; i<=matcher.groupCount(); i++) {
+//				        String groupStr = matcher.group(i);
+//				        out.print(groupStr+" ");
+//				    }
+				    out.print(matcher.group(1)+" ");
+				    out.println(matcher.group(2));
+				}
+
+	    }
+
+
+	    br.close();
+	    file.delete();
+
+
+	    out.flush();
+		out.close();
+
+		 */
+	}
 
 }
--- WEB-INF/classes/servlet/Transection.java
+++ WEB-INF/classes/servlet/Transection.java
@@ -22,98 +22,102 @@ import util.*;
 
 public class Transection extends HttpServlet {
 
-	/**
-	 * Constructor of the object.
-	 */
-	public Transection() {
-		super();
-	}
+    /**
+     * Constructor of the object.
+     */
+    public Transection() {
+        super();
+    }
 
-	/**
-	 * The doGet method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to get.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+    /**
+     * The doGet method of the servlet. <br>
+     *
+     * This method is called when a form has its tag value method equals to get.
+     *
+     * @param request the request send by the client to the server
+     * @param response the response send by the server to the client
+     * @throws ServletException if an error occurred
+     * @throws IOException if an error occurred
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-				
-		response.setContentType("text/html");
-		
-		String dir=Global.dir+"transect/";
-		String ID=RandomID.getID();
 
-		
-		String imagename=dir+Global.output_section+ID+Global.output_section_type;
-		
-		String lon1,lon2,lat1,lat2,vname,date,time;
+        response.setContentType("text/html");
 
-		vname=request.getParameter("vname");
-		lon1=request.getParameter("lon1");
-		lat1=request.getParameter("lat1");
-		lon2=request.getParameter("lon2");
-		lat2=request.getParameter("lat2");
-		date=request.getParameter("date");
-		time=request.getParameter("time");
-		
+        String dir=Global.dir+"transect/";
+        String ID=RandomID.getID();
 
-		
-		ServletContext sc = getServletContext();
 
-		
-		try{
-			FileWriter f_vname=new FileWriter(dir+Global.input_section_1+ID);
-			f_vname.write(vname+"\n");
-			f_vname.close();
-			
-			FileWriter f_lon_lat_sectoin=new FileWriter(dir+Global.input_section_2+ID);
-			f_lon_lat_sectoin.write(lon1+" "+lat1+"\n");
-			f_lon_lat_sectoin.write(lon2+" "+lat2+"\n");
-			f_lon_lat_sectoin.close();
-			
-			FileWriter f_sectiontime=new FileWriter(dir+Global.input_section_3+ID);
-			f_sectiontime.write(date.substring(0, 4)+" "+date.substring(4, 6)+" "+date.substring(6)+" "+time+"\n");
-			f_sectiontime.close();
-			
-			int timeout=0;
-			String line;
-			Process p = Runtime.getRuntime().exec("bash "+Global.bash_section+" "+ID, null, new File(dir));
-			
-			
-			
-			
-		     BufferedReader reader_e =
-		          new BufferedReader
-		            (new InputStreamReader(p.getErrorStream()));
-		        while ((line = reader_e.readLine()) != null ) {
-		        		//Thread.sleep(1000);
-		        	//new Log().log(line);
-		        		timeout++;
-		        		//System.out.println(line);
-		        }
-			
-			
-			
-		     BufferedReader reader =
-	          new BufferedReader
-	            (new InputStreamReader(p.getInputStream()));
-	        while ((line = reader.readLine()) != null ) {
-	        		//Thread.sleep(1000);
-	        		timeout++;
-	        		System.out.println(line);
-	        }
-	        
-			p.waitFor();
+        String imagename=dir+Global.output_section+ID+Global.output_section_type;
 
-//	        
+        String lon1,lon2,lat1,lat2,vname,date,time;
+
+        vname=request.getParameter("vname");
+        lon1=request.getParameter("lon1");
+        lat1=request.getParameter("lat1");
+        lon2=request.getParameter("lon2");
+        lat2=request.getParameter("lat2");
+        date=request.getParameter("date");
+        time=request.getParameter("time");
+
+
+
+        ServletContext sc = getServletContext();
+
+
+        try{
+            FileWriter f_vname=new FileWriter(dir+Global.input_section_1+ID);
+            f_vname.write(vname+"\n");
+            f_vname.close();
+
+            FileWriter log = new FileWriter("/home/smnatara/log");
+            log.write(dir+Global.input_section_1+ID+"\n");
+
+            FileWriter f_lon_lat_sectoin=new FileWriter(dir+Global.input_section_2+ID);
+            f_lon_lat_sectoin.write(lon1+" "+lat1+"\n");
+            f_lon_lat_sectoin.write(lon2+" "+lat2+"\n");
+            f_lon_lat_sectoin.close();
+
+            FileWriter f_sectiontime=new FileWriter(dir+Global.input_section_3+ID);
+            f_sectiontime.write(date.substring(0, 4)+" "+date.substring(4, 6)+" "+date.substring(6)+" "+time+"\n");
+            f_sectiontime.close();
+
+            int timeout=0;
+            String line;
+            Process p = Runtime.getRuntime().exec("bash "+Global.bash_section+" "+ID, null, new File(dir));
+            log.write("bash "+Global.bash_section+" "+ID+"\n");
+            log.write(dir+Global.output_section+ID+Global.output_section_type);
+            log.close();
+
+
+            BufferedReader reader_e =
+                    new BufferedReader
+                            (new InputStreamReader(p.getErrorStream()));
+            while ((line = reader_e.readLine()) != null ) {
+                //Thread.sleep(1000);
+                //new Log().log(line);
+                timeout++;
+                //System.out.println(line);
+            }
+
+
+
+            BufferedReader reader =
+                    new BufferedReader
+                            (new InputStreamReader(p.getInputStream()));
+            while ((line = reader.readLine()) != null ) {
+                //Thread.sleep(1000);
+                timeout++;
+                System.out.println(line);
+            }
+
+            p.waitFor();
+
 //
-//	        
-	    reader.close();
+//
+//
+            reader.close();
 
 //		    if (timeout==Global.MaxTimeout){
 //		    	//p.destroy();
@@ -122,71 +126,71 @@ public class Transection extends HttpServlet {
 //			    writer.close();
 //		    	return;
 //		    }
-		}catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		
-		 
-		
-	 // Get the MIME type of the image
-	    String mimeType = sc.getMimeType(imagename);
-	    if (mimeType == null) {
-	        sc.log("Could not get MIME type of "+imagename);
-	        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-	        return;
-	    }
-	    
-	 // Set content type
-	    response.setContentType(mimeType);
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
 
-	    // Set content size
-	    
-	    
-	    File file = new File(imagename);
-	    
-	    //busy waiting for the image
-	    int timeout=0;
-	    while(!file.exists() && timeout<Global.MaxTimeout )
-	    {
-	    	try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-	    	timeout++;
-	    }
-	    if (timeout>=Global.MaxTimeout)	return;
-	    
-	    response.setContentLength((int)file.length());
 
-	    // Open the file and output streams
-	    FileInputStream in = new FileInputStream(file);
-    
-	    // Copy the contents of the file to the output stream
-	    byte[] buf = new byte[2048];
-	    int count = 0;
-	    ServletOutputStream sout= response.getOutputStream();
-	    while ((count = in.read(buf)) >= 0) {
-	        sout.write(buf, 0, count);
-	    }
-	    
-	    in.close();
 
-	    
-	    //new File(dir+Global.input_section_1+ID).delete();
-	    //new File(dir+Global.input_section_2+ID).delete();
-	    //new File(dir+Global.input_section_3+ID).delete();
+        // Get the MIME type of the image
+        String mimeType = sc.getMimeType(imagename);
+        if (mimeType == null) {
+            sc.log("Could not get MIME type of "+imagename);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
 
-	    
-	    //file.delete();
-	    
-	    sout.close();
-	    
-	    
-		
-		
-		
-	}
+        // Set content type
+        response.setContentType(mimeType);
+
+        // Set content size
+
+
+        File file = new File(imagename);
+
+        //busy waiting for the image
+        int timeout=0;
+        while(!file.exists() && timeout<Global.MaxTimeout )
+        {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            timeout++;
+        }
+        if (timeout>=Global.MaxTimeout)	return;
+
+        response.setContentLength((int)file.length());
+
+        // Open the file and output streams
+        FileInputStream in = new FileInputStream(file);
+
+        // Copy the contents of the file to the output stream
+        byte[] buf = new byte[2048];
+        int count = 0;
+        ServletOutputStream sout= response.getOutputStream();
+        while ((count = in.read(buf)) >= 0) {
+            sout.write(buf, 0, count);
+        }
+
+        in.close();
+
+
+        //new File(dir+Global.input_section_1+ID).delete();
+        //new File(dir+Global.input_section_2+ID).delete();
+        //new File(dir+Global.input_section_3+ID).delete();
+
+
+        //file.delete();
+
+        sout.close();
+
+
+
+
+
+    }
 
 }
