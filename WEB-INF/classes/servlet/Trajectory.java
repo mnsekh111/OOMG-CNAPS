10fba53d0b257fe67c1b339c6ac342dcf99bd08e
mnsekh111 <mnsekh111@gmail.com>
2016-06-24 15:46:06 -0400

    FIxed (Process. waitFor added and response contentLength..removed)

--- WEB-INF/classes/servlet/Trajectory.java
+++ WEB-INF/classes/servlet/Trajectory.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,6 +10,9 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -41,10 +45,9 @@ public class Trajectory extends HttpServlet {
      * @throws IOException if an error occurred
      */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException {
 
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
 
         List<String> lonlats = new LinkedList<String>();
         int n = Integer.valueOf(request.getParameter("n"));
@@ -62,21 +65,26 @@ public class Trajectory extends HttpServlet {
         String ID = RandomID.getID();
         String dir = Global.dir + "trajectory/";
 
-        FileWriter input = new FileWriter(dir + Global.input_drift + ID);
-
-        for (int i = 0; i < n; i++) {
-            input.write(request.getParameter("lon" + String.valueOf(i)));
-            input.write(" ");
-            input.write(request.getParameter("lat" + String.valueOf(i)));
-            input.write("\n");
+        FileWriter input;
+        try {
+            input = new FileWriter(dir + Global.input_drift + ID);
+            for (int i = 0; i < n; i++) {
+                input.write(request.getParameter("lon" + String.valueOf(i)));
+                input.write(" ");
+                input.write(request.getParameter("lat" + String.valueOf(i)));
+                input.write("\n");
+            }
+            input.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Trajectory.class.getName()).log(Level.SEVERE, null, ex);
         }
-        input.close();
 
-        Process p;
+        Process p = null;
 
         try {
-            String line;
             p = Runtime.getRuntime().exec("bash " + Global.bash_drift + " " + ID, null, new File(dir));
+
+            String line;
             BufferedReader reader
                     = new BufferedReader(new InputStreamReader(p.getInputStream()));
             while ((line = reader.readLine()) != null) {
@@ -84,147 +92,67 @@ public class Trajectory extends HttpServlet {
                 if (line == "end\n") {
                     break;
                 }
-                //Thread.sleep(1000);
+
             }
-
-            /*			 BufferedReader reader_e =
-			          new BufferedReader
-			            (new InputStreamReader(p.getErrorStream()));
-			        while ((line = reader_e.readLine()) != null ) {
-			        		new Log().log("error "+line);
-			        }*/
             reader.close();
-            // reader_e.close();
-/*		    if (timeout==Global.MaxTimeout){
-		    	//p.destroy();
-			    BufferedWriter writer= new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
-			    writer.write("exit\n");
-			    writer.close();
-		    	return;
-		    }*/
+        } catch (IOException ex) {
+            Logger.getLogger(Trajectory.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-        } catch (Exception e) {
-            new Log().log(e.getMessage());
-
-            return;
+        if (p != null) {
+            try {
+                p.waitFor();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Trajectory.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
 
         File file = new File(dir + Global.output_drift + ID + Global.output_drift_type);
-
-
-        /*
- * This snippet of code is WRONG because the existence of output file doesn't mean all the result have been computed and written to the file
- *
- 		int timeout=0;
-		while(!file.exists() && timeout<Global.MaxTimeout )
-		{
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			timeout++;
-			new Log().log(String.valueOf(timeout));
-		}
-		if (timeout>=Global.MaxTimeout)
-		{
-		    BufferedWriter writer= new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
-		    writer.write("exit\n");
-		    writer.close();
-			return;
-		}*/
-        response.setContentLength((int) file.length());
         //new Log().log("length "+String.valueOf(file.length()));
 
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String line;
-        Pattern p1 = Pattern.compile("(\\S+)\\s+(\\S+)\\s+");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            PrintWriter out = response.getWriter();
+            String line;
+            Pattern p1 = Pattern.compile("(\\S+)\\s+(\\S+)\\s+");
 
-        while ((line = br.readLine()) != null) {
-            //new Log().log("line "+line);
-            if (line.length() == 0) {
-                out.println();
-            } else {
-                Matcher matcher = p1.matcher(line);
-                if (matcher.find()) {
-                    //			    for (int i=1; i<=matcher.groupCount(); i++) {
-                    //			        String groupStr = matcher.group(i);
-                    //			        out.print(groupStr+" ");
-                    //			    }
-                    out.print(matcher.group(1) + ",");
-                    out.print(matcher.group(2) + ";");
+            while ((line = br.readLine()) != null) {
+                //new Log().log("line "+line);
+                if (line.length() == 0) {
+                    out.println();
+                } else {
+                    Matcher matcher = p1.matcher(line);
+                    if (matcher.find()) {
+                        //			    for (int i=1; i<=matcher.groupCount(); i++) {
+                        //			        String groupStr = matcher.group(i);
+                        //			        out.print(groupStr+" ");
+                        //			    }
+                        System.out.println(matcher.group(1) + ",");
+                        System.out.println(matcher.group(2) + ",");
+                        out.print(matcher.group(1) + ",");
+                        out.print(matcher.group(2) + ";");
+                    }
                 }
+
             }
 
+            br.close();
+            out.flush();
+            out.close();
+            
+        } catch (FileNotFoundException fe) {
+            System.out.println(fe.getMessage());
+        } catch (IOException ex) {
+            Logger.getLogger(Trajectory.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        br.close();
-
+//        try {
+//            response.getWriter().print("LOL");
+//            response.getWriter().close();
+//        } catch (IOException ie) {
+//            ie.printStackTrace();
+//        }
         //new File(dir+Global.input_drift+ID).delete();
         //file.delete();
-        out.flush();
-        out.close();
-
-        /*
-		String lon1,lat1;
-		lon1=request.getParameter("lon1");
-		lat1=request.getParameter("lat1");
-
-		FileWriter input=new FileWriter(dir+Global.input_tracking);
-		input.write(lon1+" "+lat1+"\n");
-		input.close();
-
-		try{
-		Process p = Runtime.getRuntime().exec("bash "+Global.bash_tracking, null, new File(dir));
-		}catch (Exception e){
-			out.println("Process error");
-			out.println(e);
-		}
-
-		File file=new File(dir+Global.output_tracking);
-
-	    //busy waiting for the image
-	    int timeout=0;
-	    while(!file.exists() && timeout<Global.MaxTimeout )
-	    {
-	    	try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-	    	timeout++;
-	    }
-	    if (timeout>=Global.MaxTimeout)
-	    	return;
-
-	    response.setContentLength((int)file.length());
-
-	    BufferedReader br=new BufferedReader(new FileReader(file));
-	    String line;
-	    Pattern p=Pattern.compile("(\\S+)\\s+(\\S+)\\s+");
-
-	    while((line=br.readLine())!=null){
-	    		Matcher matcher= p.matcher(line);
-				if (matcher.find()){
-//				    for (int i=1; i<=matcher.groupCount(); i++) {
-//				        String groupStr = matcher.group(i);
-//				        out.print(groupStr+" ");
-//				    }
-				    out.print(matcher.group(1)+" ");
-				    out.println(matcher.group(2));
-				}
-
-	    }
-
-
-	    br.close();
-	    file.delete();
-
-
-	    out.flush();
-		out.close();
-
-         */
     }
 
 }
